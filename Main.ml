8b92598e12706a21678b3ae762540aae654f6d39
Sarah Franc <sfranc@student.42.fr>
2018-11-14 19:52:38 +0100

    cleaning Main.ml

--- Main.ml
+++ Main.ml
@@ -1,15 +1,12 @@
 let () =
   try
     let filename = Sys.argv.(1) in
-    let system =
-      Parser.parse_file filename in
-      (* |> System.expand_system in *)
-    print_endline (System.string_of_system system)
-    ;
+    let system = Parser.parse_file filename in
+    print_endline (System.string_of_system system) ;
     Search.search_all system
   with
   | Failure err -> raise (Failure err)
   | Parser.Parsing_exception str -> Printf.printf "Invalid instruction near: {%s}\n" str
-  | Invalid_argument _ -> print_endline "usage: expert [filename]"
+  | Invalid_argument _ -> print_endline "usage: ./expert filename"
   | Sys_error msg -> print_endline msg
   | Graph.Contradiction_exception str -> print_endline str
