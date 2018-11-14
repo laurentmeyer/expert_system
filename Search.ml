403cf47b8dcaea4c46239ac22b76f11462f126f1
Sarah Franc <sfranc@student.42.fr>
2018-11-14 18:57:29 +0100

    cleaning

--- Search.ml
+++ Search.ml
@@ -1,90 +1,12 @@
 type 'a tree = Leaf of 'a | NodeAnd of 'a tree list | NodeOr of 'a tree * ('a tree list)
 
-type t =
-  {
-    kb : Graph.graph ;
-    (* inferred : (Graph.Facts.t * bool) list ; *)
-    queue : Graph.Facts.t list ;
-    path : Graph.Facts.t list ;
-  }
-
-
-let dummy_tree =
-  NodeOr (
-    Leaf (Graph.Facts.Fact 'E'),
-    [
-      NodeAnd
-        [
-          NodeOr (
-            Leaf (Graph.Facts.Fact 'C'),
-            [
-              NodeAnd
-                [
-                  Leaf (Graph.Facts.Fact 'A')
-                ]
-              ;
-              NodeAnd
-                [
-                  Leaf (Graph.Facts.Fact 'B')
-                  ;
-                  Leaf (Graph.Facts.Fact 'E')
-                ]
-            ]
-          )
-          ;
-          Leaf (Graph.Facts.Fact 'D')
-        ]
-    ]
-  )
-
-
-(*  **************  SEARCH  ********************** *)
-
-let unsome_list lst =
-  let rec aux l acc =
-    match l with
-    | [] -> acc
-    | Some s :: tl -> aux tl (s :: acc)
-    | None :: tl -> aux tl acc
-  in aux lst []
-
-
-let rec or_search kb f : Graph.Facts.t tree option =
-  (* verifier que pas deja dans le tree *)
-  match Graph.condition kb f with
-  | None -> None
-  | Some o when Graph.Ors.is_empty o -> Some (Leaf f)
-  | Some o -> begin
-      let rec aux l =
-        match l with
-        | [] -> None
-        | hd :: tl -> match and_search kb hd with
-          | None -> aux tl
-          | Some s -> Some s
-      in aux (Graph.Ors.elements o)
-    end
-and and_search kb a : Graph.Facts.t tree option =
-  let results = List.map (fun f -> or_search kb f) (Graph.Ands.elements a) in
-  let failure = List.mem None results in
-  if failure then None
-  (* else let unsomed = unsome_list results in *)
-   else Some (NodeAnd (unsome_list results))
-
-
-
-
-let search (t : t) q : Graph.Facts.t tree option =
-  or_search t.kb q
-
-
-
-
 (*  **************  SERIALIZATION  *************** *)
+
 let rec depth t =
   match t with
   | Leaf _ -> 1
-  | NodeAnd lst -> 1 + List.fold_left max 0 (List.map depth lst)
-  | NodeOr (f, lst) -> List.fold_left max 0 (List.map depth lst)
+  | NodeAnd lst -> List.fold_left max 0 (List.map depth lst)
+  | NodeOr (f, lst) -> 1 + List.fold_left max 0 (List.map depth lst)
 
 let make_colums d =
   let rec aux i acc =
@@ -103,76 +25,44 @@ let string_of_tree t =
   in
   make_colums (depth t) ^ "\n" ^ aux 0 t
 
-(* let string_of_inferred (inferred : (Graph.Facts.t * bool) list) =
-   let rec aux lst =
-    match lst with
-    | [] -> ""
-    | (fact, b) :: [] -> Graph.string_of_fact fact ^ " " ^ (string_of_bool b)
-    | (fact, b) :: tl -> Graph.string_of_fact fact ^ " " ^ (string_of_bool b) ^ "\n" ^ aux tl
-   in aux inferred
+(*  **************  SEARCH  ********************** *)
 
-   let string_of_search (s : t) =
-   let rec string_of_facts = function
-   | [] -> ""
-   | fact :: [] -> Graph.string_of_fact fact
-   | fact :: tl -> Graph.string_of_fact fact ^ " |> " ^ string_of_facts tl
-   in
-   "** Knowledge base:\n" ^ Graph.string_of_graph s.kb ^ "\n" ^
-   "** Inferred table:\n" ^ string_of_inferred s.inferred ^ "\n" ^
-   "** Queue:\n" ^ string_of_facts s.queue ^ "\n" ^
-   "** Path:\n" ^ string_of_facts s.path *)
+let unsome_list lst =
+  let rec aux l acc =
+    match l with
+    | [] -> acc
+    | Some s :: tl -> aux tl (s :: acc)
+    | None :: tl -> aux tl acc
+  in aux lst []
 
+let rec or_search kb f : Graph.Facts.t tree option =
+  (* verifier que pas deja dans le tree *)
+  match Graph.condition kb f with
+  | None -> None
+  | Some o when Graph.Ors.is_empty o -> Some (Leaf f)
+  | Some o -> begin
+      let rec aux l =
+        match l with
+        | [] -> None
+        | hd :: tl -> match and_search kb hd with
+          | None -> aux tl
+          | Some s -> Some (NodeOr (Leaf f, [s]))
+      in aux (Graph.Ors.elements o)
+    end
+and and_search kb a : Graph.Facts.t tree option =
+  let results = List.map (fun f -> or_search kb f) (Graph.Ands.elements a) in
+  let failure = List.mem None results in
+  if failure then None
+   else Some (NodeAnd (unsome_list results))
+
+let search (kb : Graph.graph) q : Graph.Facts.t tree option =
+  or_search kb q
 
 (*  **************  INITIALIZATION  *************** *)
 
-let list_facts (kb : Graph.graph) : Graph.Facts.t list =
-  let get_facts (conc, cond) =
-    conc :: (cond
-             |> Graph.Ors.elements
-             |> List.map Graph.Ands.elements
-             |> List.flatten)
-  in
-  let rec aux acc lst = match lst with
-    | head::tail  -> aux (get_facts head @ acc) tail 
-    | []          -> acc
-  in
-  List.sort_uniq (Graph.Facts.compare) (aux [] kb)
-
-let init_inferred kb =
-  list_facts kb
-  |> List.map (fun x -> (x, false))
-
-let init_kb (system : System.system) : Graph.graph =
-  Graph.add_truths system.rules system.truths
-
-let init_search (system : System.system) : t =
-  let knowledge_base = init_kb system
-  in
-  {
-    kb = knowledge_base;
-    (* inferred = init_inferred knowledge_base ; *)
-    queue = [] ;
-    path = [] ;
-  }
-
-let search_one (s : t) q : t =
-  (* rajouter la recherche de la contradiction ? *)
-  let s = { s with queue = [Graph.Ands.choose (Graph.Ors.choose q)] } in
-  (* print_endline (string_of_search s) ; *)
-
-  { s with path = [] }
-
-
 let search_all (system : System.system) : unit =
-  (* print_endline (string_of_tree dummy_tree) ; *)
-  let s = init_search system in
-  let result = search s (List.hd system.queries |> Graph.Ors.choose |> Graph.Ands.choose) in
+  let kb = Graph.add_truths system.rules system.truths in 
+  let result = search kb (List.hd system.queries |> Graph.Ors.choose |> Graph.Ands.choose) in
   match result with
   | None -> print_endline "echec"
   | Some tree -> print_endline (string_of_tree tree)
-
-(* let search_all (system : System.system) : unit =
-  print_endline (string_of_tree dummy_tree) ;
-  let s = init_search system in
-  let final_search = List.fold_left search_one s system.queries in
-  () *)
